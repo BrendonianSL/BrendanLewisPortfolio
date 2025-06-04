e706b6a4b9eaae2342eab46bc2e8524235548370
Brendan <brendoniansl@gmail.com>
2025-06-03 23:27:08 -0400

    Changed to relative pathing.
    Testing relative paths.

--- portfolio/src/Homepage.jsx
+++ portfolio/src/Homepage.jsx
@@ -88,7 +88,7 @@ export default function Homepage() {
         <main className='flex flex-col gap-40 w-full max-w-[56.25rem]'>
             <section className='flex flex-col gap-8 items-start'>
                 <div className='flex flex-col gap-2 items-start'>
-                    <img src='/brendan.png' />
+                    <img src='brendan.png' />
                     <h1>Hey, I'm Brendan Lewis</h1>
                     <p className='text-(--blackSecondary)'>I'm a Web Designer and Software Engineer. I transform creative, visionary concepts into user-centered masterpieces.</p>
                 </div>
@@ -116,10 +116,10 @@ export default function Homepage() {
                 <h1>About Me</h1>
                 <div className=' flex items-center justify-center w-full lg:h-[200px]'>
                     <div className='flex flex-col items-center relative w-[600px] h-full'>
-                        <img className='w-[300px] lg:w-auto animate-[float_6s_ease-in-out_infinite] lg:absolute top-0 left-[0%]' src='/floridafam.png' />
-                        <img className='w-[300px] lg:w-auto animate-[float_3s_ease-in-out_infinite] lg:absolute top-0 left-[20%]' src='/floridabrendan.png' />
-                        <img className='w-[300px] lg:w-auto animate-[float_5s_ease-in-out_infinite] lg:absolute top-0 left-[35%]' src='/graduationparents.png' />
-                        <img className='w-[300px] lg:w-auto animate-[float_4s_ease-in-out_infinite] lg:absolute top-0 left-[60%]' src='/brendanfrances.png' />
+                        <img className='w-[300px] lg:w-auto animate-[float_6s_ease-in-out_infinite] lg:absolute top-0 left-[0%]' src='floridafam.png' />
+                        <img className='w-[300px] lg:w-auto animate-[float_3s_ease-in-out_infinite] lg:absolute top-0 left-[20%]' src='floridabrendan.png' />
+                        <img className='w-[300px] lg:w-auto animate-[float_5s_ease-in-out_infinite] lg:absolute top-0 left-[35%]' src='graduationparents.png' />
+                        <img className='w-[300px] lg:w-auto animate-[float_4s_ease-in-out_infinite] lg:absolute top-0 left-[60%]' src='brendanfrances.png' />
                     </div>
                 </div>
                 <p className='text-(--blackSecondary)'>
@@ -138,13 +138,13 @@ export default function Homepage() {
                             return (
                             <a href={project.link} key={index} className='flex justify-between items-center p-4 rounded-lg lg:hover:cursor-pointer lg:hover:bg-[#E8E8E8]'>
                                 <div className='flex items-center justify-start gap-4'>
-                                    <img src={project.icon ? `/${project.icon}.png` : `/${project.name.toLowerCase()}.png`} />
+                                    <img src={project.icon ? `${project.icon}.png` : `${project.name.toLowerCase()}.png`} />
                                     <div>
                                         <h2>{project.name}</h2>
                                         <span className='text-(--blackSecondary) text-[1rem]'>{project.projectType}</span>
                                     </div>
                                 </div> 
-                                <img src='../src/assets/arrow.png' />
+                                <img src='arrow.png' />
                             </a>
                             )
                         })
@@ -158,7 +158,7 @@ export default function Homepage() {
                         techStack.map((tech, index) => {
                             return (
                                 <div key={index} className='flex flex-col items-start gap-4 bg-white rounded-lg p-8'>
-                                    <img src={`/${tech.name.toLowerCase()}.png`} alt='React Logo' />
+                                    <img src={`${tech.name.toLowerCase()}.png`} alt={`${tech.name} logo`} />
                                     <div className='flex flex-col gap-2'>
                                         <h2>{tech.name}</h2>
                                         <span className='block text-(--blackSecondary)'>{tech.description}</span>
@@ -195,7 +195,7 @@ export default function Homepage() {
                                 <div className='flex flex-col lg:flex-row justify-between gap-4 lg:gap-16'>
                                     <span>{edu.year}</span>
                                     <div className='flex flex-col gap-2'>
-                                        <h2 className=' text-(--blackPrimary)'>{edu.name} from <img className='inline-block w-[24px] h-[24px]' src={`/${edu.icon}`} alt={`${edu.name} logo`} /> {edu.institution}</h2>
+                                        <h2 className=' text-(--blackPrimary)'>{edu.name} from <img className='inline-block w-[24px] h-[24px]' src={`${edu.icon}`} alt={`${edu.name} logo`} /> {edu.institution}</h2>
                                         <div className='text-(--blackSecondary) w-full'>{edu.description}</div>
                                     </div>
                                 </div>
