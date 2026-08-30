63a4744e5b5bc2a7b230361a665e48747c932974
Brendan Lewis <78339584+BrendonianSL@users.noreply.github.com>
2026-08-29 20:46:27 -0700

    fix:Solve tiny grammar issue in footer.

--- my-app/app/_sections/Footer.tsx
+++ my-app/app/_sections/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className='bg-neutral-900 w-full flex flex-col items-center justify-center'>
             <div className='max-w-350 w-full flex flex-col gap-8 px-8 py-16'>
                 <h2 className='font-satoshi font-semibold text-[32px] lg:text-[64px] text-transparent lowercase bg-clip-text bg-linear-to-r from-white via-[#999999] to-[#999999]'>Developing Experiences For Users No Matter Where They Are</h2>
-                <p className=' text-[#929292] max-w-162.5 w-full'>{`Feel free to contact me if having any questions.`}</p>
+                <p className=' text-[#929292] max-w-162.5 w-full'>{`Feel free to contact me if you have any questions!`}</p>
                 <div className='flex gap-4'>
                     <Button theme='CTA' text='Email Me' link='mailto:bscottlewis04@gmail.com' />
                     <Button download link='/BLResume.pdf' theme='Border' text='Download CV' />
